c1c865319b033ec096c6c19b8933ca966ed96888
rheav <rheav7@gmail.com>
2024-03-14 17:16:44 -0300

    Translated to English

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import TagManager from "react-gtm-module";
 const tagManagerArgs = {
-  gtmId: "GTM-PPQXNDT7>",
+  gtmId: "GTM-PPQXNDT7",
 };
 TagManager.initialize(tagManagerArgs);
 
