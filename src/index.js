1688552405435f24ae12bf80410f1fdc802a113d
rheav <rheav7@gmail.com>
2024-03-14 16:56:54 -0300

    GTM Added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import TagManager from "react-gtm-module";
+const tagManagerArgs = {
+  gtmId: "<ID do GTM tipo (GTM-PPQXNDT7)>",
+};
+TagManager.initialize(tagManagerArgs);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
