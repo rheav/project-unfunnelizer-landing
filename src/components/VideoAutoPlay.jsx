518c8c85445556afbdc31dc5f95226807c756ab2
rheav <rheav7@gmail.com>
2024-10-12 14:11:49 -0300

    clean

--- src/components/VideoAutoPlay.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const VideoAutoPlay = ({ video }) => {
-	return <video className="mx-auto w-3/4 shadow-xl mb-10 rounded-md" src={video} type="video/mp4" autoPlay muted loop playsInline />;
-};
-
-export default VideoAutoPlay;
